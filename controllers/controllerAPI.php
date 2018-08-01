108c5ee16016084991eb5e66a423dff9acb970e1
max <lhmax55@gmail.com>
2018-07-31 23:27:59 -0400

    de chez nous

--- controllers/controllerAPI.php
+++ controllers/controllerAPI.php
@@ -1,13 +1,12 @@
 <?php
+
 // Method: POST, PUT, GET etc
 // Data: array("param" => "value") ==> index.php?param=value
 
-function CallAPI($method, $url, $data = false)
-{
+function CallAPI($method, $url, $data = false) {
     $curl = curl_init();
 
-    switch ($method)
-    {
+    switch ($method) {
         case "POST":
             curl_setopt($curl, CURLOPT_POST, 1);
 
@@ -36,3 +35,28 @@ function CallAPI($method, $url, $data = false)
     return $result;
 }
 
+function CallJSON($categorie) {
+    $results = file_get_contents("5e-database/5e-SRD-$categorie.json");
+    $results = json_decode($results, true);
+    return $results;
+}
+
+function drawIcon($iconToDraw){
+    switch($iconToDraw){
+        case "HP":
+            echo '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" width="16" height="16"><path d="M414.9 24C361.8 24 312 65.7 288 89.3 264 65.7 214.2 24 161.1 24 70.3 
+                24 16 76.9 16 165.5c0 72.6 66.8 133.3 69.2 135.4l187 180.8c8.8 8.5 22.8 8.5 31.6 0l186.7-180.2c2.7-2.7 69.5-63.5 69.5-136C560 76.9 505.7 24 414.9 24z"
+                style="fill: #FF4136;"></path></svg>';
+            break;
+        case "AC":
+            echo '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="16" height="16"><path d="M466.5 83.7l-192-80a48.15 48.15 0 0 0-36.9 0l-192 80C27.7 91.1 
+                16 108.6 16 128c0 198.5 114.5 335.7 221.5 380.3 11.8 4.9 25.1 4.9 36.9 0C360.1 472.6 496 349.3 496 128c0-19.4-11.7-36.9-29.5-44.3z" style="fill: #19A974;">
+                </path></svg>';
+            break;
+        case "Speed":
+            echo '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="16" height="16"><path d="M256 8c137 0 248 111 248 248S393 504 256 504 8 393 8 256 119 8 256
+                8zm-28.9 143.6l75.5 72.4H120c-13.3 0-24 10.7-24 24v16c0 13.3 10.7 24 24 24h182.6l-75.5 72.4c-9.7 9.3-9.9 24.8-.4 34.3l11 10.9c9.4 9.4 24.6 9.4 33.9 0L404.3
+                273c9.4-9.4 9.4-24.6 0-33.9L271.6 106.3c-9.4-9.4-24.6-9.4-33.9 0l-11 10.9c-9.5 9.6-9.3 25.1.4 34.4z" style="fill: #408BC9;"></path></svg>';
+            break;
+    }
+}
--- test.php
+++ test.php
@@ -1,20 +1,34 @@
 <?php
 include 'controllers/controllerAPI.php';
 include_once 'includes/header.php';
-$results = CallAPI("GET", "www.dnd5eapi.co/api/spells");
-$results = json_decode($results);
+$results = CallJSON("Monsters");
 //var_dump($results);
 ?>
 
-<div class="grid-x grid-margin-x small-up-2 medium-up-4 large-up-6">
+<div class="grid-x grid-margin-x small-up-2 medium-up-4 large-up-5">
     <?php
-    foreach($results->results as $key => $result) {
+    foreach ($results as $key => $result) {
         ?>
         <div class="cell ">
             <div class="card spell">
-                <div class="card-divider">
-                    <h4><?= $result->name; ?></h4>
+                <div class="card-divider ">
+                    <h4 class="title"><?= $result["name"]; ?></h4>
                 </div>
+                <div class="card-section">
+                    <span class="challenge_rating label">CR : <?= $result["challenge_rating"]; ?></span>
+                    <span class="size label"><?= ucfirst($result["size"]); ?></span>
+                    <span class="type label"><?= ucfirst($result["type"]); ?></span>
+                    <?php if (!empty($result["subtype"])) {
+                        ?><span class="subtype label secondary"><?= ucfirst($result["subtype"]); ?></span><?php }
+                    ?>
+                    <span class="alignment label"><?= ucfirst($result["alignment"]); ?></span>
+                </div>
+                <div class="card-section">
+                    <span class=""><?php drawIcon("HP"); ?><?= $result["hit_points"] . " (" . $result["hit_dice"] . ")"; ?></span>
+                    <span class=""><?php drawIcon("AC"); ?><?= $result["armor_class"]; ?></span>
+                    <span class=""><?php drawIcon("Speed"); ?><?= $result["speed"]; ?></span>
+                </div>
+
             </div>
         </div>
         <?php
