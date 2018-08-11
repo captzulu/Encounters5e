d22ae629f0a84f424a2334fd709fe7eda9feba37
max <lhmax55@gmail.com>
2018-08-11 17:50:05 -0400

    forced to commit chnages

--- includes/connectionInfo.php
+++ includes/connectionInfo.php
@@ -4,6 +4,6 @@ $options = [PDO::ATTR_EMULATE_PREPARES => false, // turn off emulation mode for 
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
 
-$db = new PDO("mysql:host=localhost;dbname=5eData;charset=utf8", "root", "captzulu55", $options);
+$db = new PDO("mysql:host=localhost;dbname=encount1_encountersData;charset=utf8", "encount1_max", "captzulu55", $options);
 
 
