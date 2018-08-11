2682c3058cac7ddce025e3a8e8eda6e387f4de10
captzulu <lhmax55@gmail.com>
2018-08-11 17:54:38 -0400

    Delete connectionInfo.php

--- includes/connectionInfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$options = [PDO::ATTR_EMULATE_PREPARES => false, // turn off emulation mode for "real" prepared statements
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
-
-$db = new PDO("mysql:host=localhost;dbname=encount1_encountersData;charset=utf8", "encount1_max", "captzulu55", $options);
-
-
